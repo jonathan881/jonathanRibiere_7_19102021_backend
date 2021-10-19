8a37f6854a769082ea92225c85e177796cea69b7
Jonathan Ribiere <ribiere88@sfr.fr>
2021-10-19 06:07:37 +0200

    mise en place des regex

--- routes/usersCtrl.js
+++ routes/usersCtrl.js
@@ -1,8 +1,18 @@
 const bcrypt = require("bcrypt");
 const jwtUtils = require("../utils/jwt.utils");
 const models = require("../models");
+const asyncLib = require("async");
 
-//Routes
+//Constants
+const EMAIL_REGEX =
+  /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+
+/*Le mot de passe doit comporter au moins 4 caractères, pas plus de 12 caractères et 
+doit inclure au moins une lettre majuscule, une lettre minuscule et un chiffre numérique.*/
+const PASSWORD_REGEX = /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{4,12}$/;
+
+//-----------------ROUTES------------------------//
+
 module.exports = {
   register: function (req, res) {
     //parametre
@@ -13,7 +23,41 @@ module.exports = {
     if (email == null || username == null || password == null) {
       return res.status(400).json({ error: "missing parameters" });
     }
-    //Ici placer les regex
+
+    if (username.length >= 15 || username.length <= 2) {
+      return res
+        .status(400)
+        .json({ error: "Le mdp dois contenire entre 3 et 14 caractères" });
+    }
+
+    if (!EMAIL_REGEX.test(email)) {
+      return res.status(400).json({ error: "E-mail Invalide!" });
+    }
+
+    /*Le mot de passe doit comporter au moins 4 caractères, pas plus de 12 caractères et 
+    doit inclure au moins une lettre majuscule, une lettre minuscule et un chiffre numérique.*/
+    if (!PASSWORD_REGEX.test(password)) {
+      return res.status(400).json({
+        error:
+          "Mots de passe Invalide! Entre 4 et 12 caractères une majuscule et un chiffre",
+      });
+    }
+
+    asyncLib.waterfall(
+      [
+        function (done) {
+          done(null, "variable1");
+        },
+      ],
+      function (err) {
+        if (!err) {
+          return res.status(200).json({ msg: "ok" });
+        } else {
+          return res.status(404).json({ error: "error" });
+        }
+      }
+    );
+
     //Verifiée si l'email existe pas dans la BDD
     models.User.findOne({
       attributes: ["email"],
