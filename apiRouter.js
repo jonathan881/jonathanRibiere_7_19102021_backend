e4bf377afdeecdbebae62c16792bee2d03a4b2b6
Jonathan Ribiere <ribiere88@sfr.fr>
2021-11-04 00:41:24 +0100

    modif route pr modif profile

--- apiRouter.js
+++ apiRouter.js
@@ -15,8 +15,8 @@ exports.router = (function () {
   apiRouter.route("/users/register/").post(usersCtrl.register);
   apiRouter.route("/users/login/").post(usersCtrl.login);
   apiRouter.route("/users/profile/").get(usersCtrl.getUserProfile);
-  apiRouter.route("/users/profile/").delete(usersCtrl.deleteProfile);
-  apiRouter.route("/users/profile/").put(usersCtrl.updateUserProfile);
+  apiRouter.route("/users/delete/").delete(usersCtrl.deleteProfile);
+  apiRouter.route("/users/update/").put(usersCtrl.updateUserProfile);
 
   //Routes pour les Messages
   apiRouter.route("/messages/new/").post(multer, messagesCtrl.createMessage);
